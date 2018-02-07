ba0708b7bbbacd470554ec5602eac385a6274363
dave-roo <35938373+dave-roo@users.noreply.github.com>
2018-02-07 22:47:05 +0100

    changes to featureNormalize

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,18 +26,12 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
 mu = mean(X_norm);
 sigma = std(X_norm);
 
 tf_mu = X_norm - repmat(mu,length(X_norm),1);
 tf_std = repmat(sigma,length(X_norm),1);
-
 X_norm = tf_mu ./ tf_std;
-
-
-
 
 
 % ============================================================
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -18,12 +18,13 @@ for iter = 1:num_iters
     %
 
 
-
-
     delta = (1/m)*sum(X.*repmat((X*theta - y), 1, size(X,2)));
     
     
     theta = (theta' - (alpha * delta))';
+
+
+
 
 
 
